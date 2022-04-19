6f70e8b37ee7a6145d5434c12c8c725ffd6a8261
michmakarov <michmakarov@gmail.com>
2022-04-19 10:08:48 +0300

    As if the mmclient works properlywith registration

--- mmclient/mncln.go
+++ mmclient/mncln.go
@@ -4,6 +4,7 @@
 //So, from overview of  https://pkg.go.dev/net/http :
 //For control over proxies, TLS configuration, keep-alives, compression, and other settings, create a Transport: ...
 //Also from https://stackoverflow.com/questions/38822764/how-to-send-a-https-request-with-a-certificate-golang; second answer
+//220418 13:46 if the func setArgs() returns false the func setAgrsFromFile() will be invoked
 package main
 
 import (
@@ -20,37 +21,34 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"io/ioutil"
-	"log"
+
+	"bufio"
 )
+
+const OptionsFile = "options.txt"
 
 var defurl string = "https://www.dost346.ru:8080/q" // default value if there is not an argument
 var defmeth = "get"
+var defcookies string //a default is absent of cookies
 
 var help = `
-220416 09:17
-It is http(s) CLI client that regards the answer as a plain text and print it to stdout
-A command line is :
-prog urg1, urg2, ... urgN
-where
-prog is a reference to this programm, for example ./mmclient
-urg<number> is a command line agrument with format of <option name>=<option value>, for example URL="https://dost346.ru:8080/qqq?xxx=y zzz"
-As usual, the option value must be enslosed by quotes if it contains blank characters.
-Arguments are not mandatory. That is for each there is a default value.
+220416 09:17 220419 07:33
+It is http(s) CLI client that makes a http(s) requst and prints result to to stdout
+A command lines are :
+prog
+prog h
+where "prog" is a call of this programm, for example ./mmclient
+If there is the argument "h" (that is when "prog h") the program print (to console) this text and ended its work with code 0.
+Otherwise the program demands existing in its working directory a file of "options.txt" from which it takes its options.
 
 There are options (the case is significant):
-h - it is exclusive option. If it is in command line the prog ignoring all and only print this help text.
 URI - uri, by which the prog answers a http(s) server, for example URL="https://www.dost346.ru:8080/qqq?xxx=y zzz";
-default value - https://dost346.ru:8080/ind
-METH - a method of a request, for example METH=post;
-default value - get
-
-If in the working directory of the prog there is file "options.txt" the prog prefer optionss from it than from the command line.
-That is the prog reading its option from the file (excluding the option "h").
-
+METH - a method of a request, for example METH=get;
+COOKIES - a cookies that will sent with a requst
 RETURNS:
-if there is option "h" (with any value) the prog returns 0 and prints this text.
-Otherwise it questions a server;
-if is any answer from the server it returns 0 and print the answer, overwise it returns 1 and primts error message.
+If there is option "h" the prog returns 0
+Otherwise it returns 0 if sending a request takes place and 1 if no request was sent
+In any case the program print a mesage to console.
 `
 
 func printResp(resp *http.Response) {
@@ -69,7 +67,9 @@ func printResp(resp *http.Response) {
 
 //220416 09:42 It is from the mmst. This scheme seems very good.
 //That is it allows arbitrary amount of arguments and only one format of a command line.
-func setArgs() {
+/* 220419 refused
+func setArgs() bool {
+	var res bool
 	for i := 1; i < len(os.Args); i++ {
 		var splitedArg = strings.Split(os.Args[i], "=")
 		if len(splitedArg) != 2 {
@@ -93,18 +93,94 @@ func setArgs() {
 			fmt.Printf("There is not allowed option %v\n", splitedArg[0])
 			os.Exit(1)
 		}
+		res = true // if the switch did not halt the prog then the switch has establish a valid option
 	}
+	return res
+}
+*/
+
+//220419 08:52
+func toTwo(sl []string, link string) []string {
+	var res []string = make([]string, 2)
+	var rest string
+	switch len(sl) {
+	case 0, 1:
+		panic("toTwo: sourse slice len = 0 or 1")
+	case 2:
+		return sl
+	}
+	for i := 1; i < len(sl); i++ {
+		if i == 1 {
+			rest = rest + sl[i]
+		} else {
+			rest = rest + link + sl[i]
+		}
+	}
+	res[0] = sl[0]
+	res[1] = rest
+	return res
+}
+
+//220418 13:46
+func setAgrsFromFile() {
+	var err error
+	var line string
+	var f *os.File
+	var sc *bufio.Scanner
+
+	var optSlice []string
+
+	//printDebug("showFeelerLogHandler: srart")
+
+	if f, err = os.Open(OptionsFile); err != nil {
+		panic(fmt.Sprintf("setAgrsFromFile: open %s err=%s", OptionsFile, err.Error()))
+	}
+	sc = bufio.NewScanner(f)
+	for sc.Scan() {
+		line = sc.Text()
+		if line[0] != byte('#') {
+			optSlice = strings.Split(line, "=")
+			if len(optSlice) == 1 {
+				panic(fmt.Sprintf("setAgrsFromFile: line %s is not comment and has not = character", line))
+			}
+			if len(optSlice) > 2 {
+				//panic(fmt.Sprintf("setAgrsFromFile: line %s has more than one = character", line))
+				optSlice = toTwo(optSlice, "=")
+			}
+			switch optSlice[0] {
+			case "URI":
+				defurl = optSlice[1]
+			case "COOKIES":
+				defcookies = optSlice[1]
+			default:
+				panic(fmt.Sprintf("setAgrsFromFile: line %s has illegal option %s", line, optSlice[0]))
+			}
+			//fmt.Println("--------" + optSlice[1])
+
+		}
+	}
+	if err = sc.Err(); err != nil {
+		panic(fmt.Sprintf("setAgrsFromFile: scanning %s", err.Error()))
+	}
+
 }
 
 func main() {
-	//var err error
-	//var res string
-	setArgs() //if there is "h" option that the prog will halt
+	if len(os.Args) > 1 { //if there is "h" option that the prog will halt
+		if os.Args[1] != "h" {
+			panic("Allowed only h argument")
+		} else {
+			fmt.Println(help)
+			os.Exit(0)
+		}
+	}
+	setAgrsFromFile()
 
 	//220418 08:12
 	caCert, err := ioutil.ReadFile("cert")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Readig cart file err=%s\n", err.Error())
+		os.Exit(1)
 	}
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
@@ -117,8 +193,20 @@ func main() {
 		},
 	}
 	client := &http.Client{Transport: tr}
-	if resp, err := client.Get(defurl); err != nil {
-		fmt.Printf("Getting %s err=%s\n", defurl, err.Error())
+	//220419 06:19
+	req, err := http.NewRequest("GET", defurl, nil)
+	if err != nil {
+		fmt.Printf("http.NewRequest (for %s) err=%s\n", defurl, err.Error())
+		os.Exit(1)
+	}
+	if defcookies != "" {
+		req.Header.Add("cookie", defcookies)
+	}
+
+	//req.Header.Add()
+
+	if resp, err := client.Do(req); err != nil {
+		fmt.Printf("client.Do (for %s) err=%s\n", defurl, err.Error())
 		os.Exit(1)
 	} else {
 		printResp(resp)
